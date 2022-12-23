2df72865f15798b6af8e7d0d3d57cd728efc1ce4
vazhab <vazha.bichiashvili@gmail.com>
2022-12-23 16:26:34 +0400

    updated app.js

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import ViewImageScreen from './src/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
+    <ViewImageScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-});
+const styles = StyleSheet.create({})
