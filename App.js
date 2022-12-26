e7f1d5bf25be3985ebe2870763e0545d7afbe3f3
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-12-26 12:19:20 +0400

    Listing details screen implementation
    - created ListItem reusable component
    - edited colors.js
    - added icons to ViewImageScreen
    - implemented Listing details screen

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import WelcomeScreen from './src/screens/WelcomeScreen';
+import ListingDetailsScreen from './src/screens/ListingDetailsScreen';
 import ViewImageScreen from './src/screens/ViewImageScreen';
-import Card from './src/components/Card';
 
 export default function App() {
-  return (
-    //<WelcomeScreen />
-    <View style={styles.main}>
-      <Card
-        title={'someting for sale'}
-        subTitle={'$ 1000'}
-        image={require("./assets/jacket.jpg")} />
-    </View>
-  );
+  return <ViewImageScreen />
 }
 
-const styles = StyleSheet.create({
-  main: {
-    backgroundColor: '#f8f3f3',
-    padding: 20,
-    paddingTop: 100,
-  },
-})
