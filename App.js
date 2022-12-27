dedcd03c8cc262df5d41057e97e94e1f583587f1
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-12-27 06:18:57 +0400

    Messages screen added
    - added reusable component to manage notification bar padding\margin
    - Messages screen added

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ListingDetailsScreen from './src/screens/ListingDetailsScreen';
 import ViewImageScreen from './src/screens/ViewImageScreen';
+import MessagesScreen from './src/screens/MessagesScreen';
 
 export default function App() {
-  return <ViewImageScreen />
+  return <MessagesScreen />
 }
 
