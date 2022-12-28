e6b8e19099bcd8b59892986cbfe2cbc31ee66fd7
vazhab <vazha.bichiashvili@gmail.com>
2022-12-29 00:27:24 +0400

    Updated/cleaned project
    - solved building error on mac

--- App.js
+++ App.js
@@ -14,10 +14,11 @@ import colors from './src/config/colors';
 import { Switch } from 'react-native';
 import AppPicker from './src/components/AppPicker';
 import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen'
 
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
 
 const styles = StyleSheet.create({
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -5,63 +5,63 @@ import { AppForm, AppFormField, SubmitButton } from '../components/forms'
 import Screen from "../components/Screen";
 
 const validationSchema = Yup.object().shape({
-    name: Yup.string().required().label("Name"),
-    email: Yup.string().required().email().label("Email"),
-    password: Yup.string().required().min(4).label("Password")
+  name: Yup.string().required().min(2).label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password")
 })
 
 function RegisterScreen(props) {
 
-    return (
-        <Screen style={styles.container}>
-            <AppForm
-                initialValues={{ name: '', email: '', password: '' }}
-                onSubmit={values => console.log(values)}
-                validationSchema={validationSchema}>
-                <AppFormField
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    icon="user"
-                    keyboardType=""
-                    name="email"
-                    placeholder="Email"
-                    textContentType="emailAddress"
-                />
-                <AppFormField
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    icon="email"
-                    keyboardType="email-address"
-                    name="email"
-                    placeholder="Email"
-                    textContentType="emailAddress"
-                />
-                <AppFormField
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    icon="lock"
-                    name="password"
-                    placeholder="Password"
-                    textContentType="Password"
-                    secureTextEmtry
-                />
-                <SubmitButton title="Login" />
-            </AppForm>
-        </Screen>
-    );
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{ name: '', email: '', password: '' }}
+        onSubmit={values => console.log(values)}
+        validationSchema={validationSchema}>
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false} s
+          icon="account"
+          keyboardType="default"
+          name="name"
+          placeholder="Username"
+          textContentType="name"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          textContentType="Password"
+          secureTextEmtry
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-    },
-    logo: {
-        width: 80,
-        height: 80,
-        alignSelf: "center",
-        marginTop: 50,
-        marginBottom: 20,
-    }
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  }
 
 });
 
