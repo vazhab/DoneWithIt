bb7d3ffbd05139473cd6eb6eca2528acaaa4a21a
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-12-29 17:08:45 +0400

    minor changes
    - removed package-lock.json
    - added permission request method

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Screen from './src/components/Screen';
 import ListingDetailsScreen from './src/screens/ListingDetailsScreen';
@@ -16,10 +16,24 @@ import AppPicker from './src/components/Picker';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen'
 import ListingEditScreen from './src/screens/ListingEditScreen';
-
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
-  return <ListingEditScreen />
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (!granted) {
+      alert('You need to enable permission to access the library')
+    }
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
+  return <RegisterScreen />
+  //<Screen></Screen>
 }
 
 const styles = StyleSheet.create({
