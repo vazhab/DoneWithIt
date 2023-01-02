54b307bd34f0602be28c6c20a1ce03329de6e167
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2023-01-02 07:26:27 +0400

    ImageInput List and remove/add image function added

--- App.js
+++ App.js
@@ -19,36 +19,26 @@ import ListingEditScreen from './src/screens/ListingEditScreen';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import ImageInput from './src/components/ImageInput';
+import ImageInputList from './src/components/ImageInputList';
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync()
-    if (!granted) {
-      alert('You need to enable permission to access the library')
-    }
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
   }
 
-  useEffect(() => {
-    requestPermission();
-  }, [])
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.canceled) {
-        setImageUri(result.assets[0].uri)
-      }
-    } catch (error) {
-      console.log('Error reading an Image', error);
-    }
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
   }
 
   return (
     <Screen>
-      <ImageInput imageUri={imageUri}
-        onChangeImage={uri => setImageUri(uri)} />
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
     </Screen>
   );
 }
