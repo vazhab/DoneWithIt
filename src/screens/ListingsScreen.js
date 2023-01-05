bac085b2a8ee85beb9da7c4e11244396e3abc61c
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2023-01-05 17:15:16 +0400

    get listings from API Added
    - apisauce installed

--- src/screens/ListingsScreen.js
+++ src/screens/ListingsScreen.js
@@ -1,36 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Screen from "../components/Screen";
 import { FlatList, StyleSheet } from "react-native";
 import Card from '../components/Card';
 import colors from "../config/colors";
 import routes from "../navigation/routes";
-
-const Listings = [
-  {
-    id: 1,
-    title: 'Red jacket for sale',
-    price: 100,
-    image: require('../../assets/jacket.jpg'),
-  },
-  {
-    id: 2,
-    title: 'Couch in great condition',
-    price: 1000,
-    image: require('../../assets/couch.jpg'),
-  },
-]
+import listingsApi from "../api/listings"
 
 function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
-        data={Listings}
-        keyExtractor={listing => listing.id.toString()}
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
         renderItem={({ item }) =>
           <Card
             title={item.title}
             subTitle={'$' + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         }
